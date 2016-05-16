914e476d29d5f20dda95e27770d9ff1ca8364bda
87maza <87maza@gmail.com>
2016-05-15 22:21:10 -0700

    using coligo method

--- server.js
+++ server.js
@@ -10,22 +10,32 @@ app.use(morgan('short'));
 
 mongoose.connect("mongodb://admin:admin@ds021922.mlab.com:21922/shorty");
 
-app.get('/', function(req,res){
-	fs.createReadStream(__dirname + '/index.html').pipe(res);
-})
+app.use(express.static('public'));
+//home route style
 
-app.get('/:web', function(req,res){
-    var web = req.params.web
-    if(web.substr(0,5)==='https' ||web.substr(0,5)==='http:'){
-        var data = {
-            original: req.params.web,
-            shorty: "shawty"
-        }
-        res.json(data);
-    }
-    res.send('please use proper url-formatting with "http://www.example.com/" or "https://www.example.com/"')
+// app.get('/:web', function(req,res){
+//     var web = req.params.web
+//     if(web.substr(0,5)==='https' ||web.substr(0,5)==='http:'){
+//         var data = {
+//             original: req.params.web,
+//             shorty: "shawty"
+//         }
+//         res.json(data);
+//     }
+//     res.send('please use proper url-formatting with "http://www.example.com/" or "https://www.example.com/"')
     
-})
+// })
+
+
+app.post('/api/shorten', function(req, res){
+  // route to create and return a shortened URL given a long URL
+});
+
+app.get('/:encoded_id', function(req, res){
+  // route to redirect the visitor to their original URL given the short URL
+});
+
+
 
 app.listen(port, function(){
 	console.log('runnin on ' + port)
