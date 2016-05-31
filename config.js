f9384429badd7cc2e2bedec274a2cdd66beb9104
87maza <87maza@gmail.com>
2016-05-30 22:11:52 -0700

    added missing backslash argggh

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.db = {};
 
 // config.webhost = 'localhost:3000/';
-config.webhost = "https://smlr.herokuapp.com"
+config.webhost = "https://smlr.herokuapp.com/"
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
